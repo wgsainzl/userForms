8d80fa63ccd275cf79c09e8e12dfd0c3d4192d3e
wgsainzl <wgsainzl@gmail.com>
2025-04-23 16:22:13 -0600

    Log In

--- users-forms/src/Login.jsx
+++ users-forms/src/Login.jsx
@@ -1,50 +1,71 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import img1 from "./images/Wallpaper.jpeg";
 
 const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [form, setForm] = useState({
-        email: '',
-        password: ''
-    });
+  const handleChange = (e) => {
+    const { value, name } = e.target;
 
-    const handleChange = (e) => {
-        const { value, name } = e.target;
+    console.log(value);
+    console.log(name);
 
-        console.log(value);
-        console.log(name);
+    const newForm = {
+      ...form,
+      [name]: value,
+    };
 
-        const newForm = {
-            ...form,
-            [name]: value
-        }
-
-        console.log(newForm)
-        setForm(newForm)
-    }
+    console.log(newForm);
+    setForm(newForm);
+  };
 
   return (
     <>
-    <div>Login</div>
-    <label>
-        Email
-        <input 
-        type="text"
-        name="email"
-        onChange={handleChange}
-        value={form.name}
-        />
-    </label>
-    <label>
-        Password 
-        <input 
-        type="text"
-        name="password"
-        onChange={handleChange}
-        value={form.name}
-        />
-    </label>
+      <div className="registerContainer">
+        <div className="leftContainer">
+          <img src={img1} alt="img1" className="image" />
+        </div>
+        <div className="rightContainer">
+          <h1 className="tittleLog">Log In</h1>
+          <label className="labelInput">
+            Email
+            <input
+              className="inputEmail"
+              type="text"
+              name="email"
+              onChange={handleChange}
+              value={form.name}
+            />
+          </label>
+          <label className="labelInput">
+            Password
+            <input
+              className="inputPassword"
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={form.name}
+            />
+          </label>
+          <label className="labelCheckbox">
+            <input
+              className="inputCheckbox"
+              type="checkbox"
+              name="checkbox"
+              onChange={handleChange}
+              value={form.name}
+            />
+            I am not a robot
+          </label>
+          <button className="registerButton">Log In</button>
+          <hr></hr>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
